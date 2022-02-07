c4cf00ac36eeb1b8b97b815d6472a263f17ffaca
Jinkang-Lee <68258103+Jinkang-Lee@users.noreply.github.com>
2022-02-08 01:30:20 +0800

    JK Push

--- Currency Exhange/wwwroot/js/Code.js
+++ Currency Exhange/wwwroot/js/Code.js
@@ -9,6 +9,15 @@
 /*Loop through the list of countries to display the countries the CONSOLE*/
 for (let i = 0; i < dropList.length; i++) {
     for (let currency_code in country_list) {
-        console.log(currency_code)
+
+        //By default set currency selected to USD and NPR
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "NPR" ? "selected" : "";
+
+        //Dropdown box to see all the currencies available
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
-}
+    dropList[i].addEventListener("change", e => {
+        loadFlag(e.target);
+    })
+};
